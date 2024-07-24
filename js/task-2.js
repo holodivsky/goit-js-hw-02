e23664ba4769c99cf4c81e7b87721121a3ed12fb
Volodymyr Holodivsky <volodymyrholodivsky@MacBook-Air-1.local>
2024-07-24 11:48:28 +0200

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 'use strict';
 function formatMessage(message, maxLength) {
-    return message.length > maxLength ? message : `${message.slice(0,maxLength)}...`;
+    return message.length <= maxLength ? message : `${message.slice(0,maxLength)}...`;
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
